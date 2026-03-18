be4ec703ce004db63d1401d1eddafad234985b29
vveerrgg <vergel@humanjava.com>
2026-03-18 08:07:10 -0700

    chore: bump to 0.2.1 for PyPI + ClawHub release

--- src/nostrkey/__init__.py
+++ src/nostrkey/__init__.py
@@ -4,7 +4,7 @@ from nostrkey.identity import Identity
 from nostrkey.keys import generate_keypair, nsec_to_hex, npub_to_hex, hex_to_nsec, hex_to_npub
 from nostrkey.seed import generate_seed_phrase, validate_seed_phrase, seed_phrase_to_private_key
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "Identity",
     "generate_keypair",
